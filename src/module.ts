8249041eaa94a331f87a659b26d25b1b5834579e
hywax <a.hywax@gmail.com>
2025-06-14 18:02:34 +0500

    feat: module imports

--- src/module.ts
+++ src/module.ts
@@ -1,20 +1,43 @@
-import { defineNuxtModule } from '@nuxt/kit'
+import { addComponentsDir, addImports, createResolver, defineNuxtModule } from '@nuxt/kit'
+import { defu } from 'defu'
 import { name, version } from '../package.json'
 
 export interface ModuleOptions {
-  url: string
+  fluxorUrl?: string
+  prefix?: string
 }
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
     name,
     version,
-    configKey: 'fluxor',
+    configKey: 'uplora',
   },
   defaults: {
-    url: 'https://fluxor.uplora.ru',
+    fluxorUrl: 'https://fluxor.uplora.ru',
+    prefix: 'Uplora',
   },
-  async setup() {
-    // ...
+  async setup(options, nuxt) {
+    const { resolve } = createResolver(import.meta.url)
+
+    nuxt.options.runtimeConfig.public.uplora = defu(nuxt.options.runtimeConfig.public.uplora as any, {
+      fluxorUrl: options.fluxorUrl,
+    })
+
+    const runtimeDir = resolve('./runtime')
+    nuxt.options.alias['#uplora'] = runtimeDir
+    nuxt.options.build.transpile.push(runtimeDir)
+
+    addComponentsDir({
+      path: resolve('./runtime/components'),
+      prefix: options.prefix,
+      pathPrefix: false,
+      global: true,
+    })
+
+    addImports({
+      name: `use${options.prefix}Image`,
+      from: resolve('runtime/composables/useImage'),
+    })
   },
 })
