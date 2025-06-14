46953ed6f5acce351bf79885d5c8c635eeb17332
hywax <a.hywax@gmail.com>
2025-06-14 15:40:07 +0500

    feat: enhance image component with LQIP, preload, and error handling

--- src/runtime/components/Image.vue
+++ src/runtime/components/Image.vue
@@ -1,22 +1,51 @@
 <template>
   <div :class="ui.root({ class: props.ui?.root })">
+    <div
+      v-if="lqip"
+      :class="ui.lqip({ class: props.ui?.lqip })"
+      :style="{
+        backgroundImage: lqip.image,
+        backgroundColor: lqip.color,
+      }"
+    />
     <picture :class="ui.picture({ class: props.ui?.picture })">
-      ...
+      <template v-if="image.sources && image.sources.length > 1">
+        <source
+          v-for="(source, key) in image.sources"
+          :key="key"
+          :srcset="source.srcset"
+          :type="source.type"
+        >
+      </template>
+
+      <img
+        ref="image"
+        v-bind="imageAttrs"
+        :src="image.img"
+        :srcset="image?.srcset"
+        :alt="alt"
+        :loading="loading"
+        :class="ui.img({ class: props.ui?.img })"
+      >
     </picture>
   </div>
 </template>
 
 <script lang="ts">
-import type { Format, Size } from '../types/formats'
+import type { SerializableHead } from '@unhead/vue/types'
+import type { ImgHTMLAttributes } from 'vue'
+import type { Format, LQIP, Size } from '../types/formats'
 import type { ComponentSlots } from '../types/tv'
-import { computed } from '#imports'
+import { computed, onMounted, useHead, useNuxtApp, useTemplateRef } from '#imports'
 import { tv } from 'tailwind-variants'
 import { useImage } from '../composables/useImage'
 
 const uploraImage = tv({
   slots: {
     root: 'relative',
+    lqip: 'bg-cover bg-no-repeat bg-center',
     picture: '',
+    img: '',
   },
 })
 
@@ -25,13 +54,18 @@ export interface UploraImageProps {
   alt: string
   formats?: Format[]
   sizes?: Size[]
+  lqip?: LQIP
   loading?: 'lazy' | 'eager'
+  preload?: boolean | { fetchPriority: 'auto' | 'high' | 'low' }
+  // csp
+  nonce?: string
+  imgAttrs: ImgHTMLAttributes
   ui?: ComponentSlots<typeof uploraImage>
 }
 
 export interface UploraImageEmits {
   load: [Event]
-  error: [Event]
+  error: [string | Event]
 }
 </script>
 
@@ -39,13 +73,63 @@ export interface UploraImageEmits {
 const props = withDefaults(defineProps<UploraImageProps>(), {
   loading: 'lazy',
 })
+const emit = defineEmits<UploraImageEmits>()
 
-defineEmits<UploraImageEmits>()
+const nuxtApp = useNuxtApp()
+const initialLoad = nuxtApp.isHydrating
 
-const _image = useImage({
+const image = computed(() => useImage({
   id: props.src,
   formats: props.formats,
   sizes: props.sizes,
+}))
+
+if (import.meta.server && props.preload) {
+  useHead({ link: () => {
+    if (!image.value.img) {
+      return []
+    }
+
+    const link: NonNullable<SerializableHead['link']>[number] = {
+      rel: 'preload',
+      as: 'image',
+      imagesrcset: image.value.srcset,
+      nonce: props.nonce,
+      ...(typeof props.preload !== 'boolean' && props.preload?.fetchPriority
+        ? { fetchpriority: props.preload.fetchPriority }
+        : {}),
+    }
+
+    return [link]
+  } })
+}
+
+const imageRef = useTemplateRef('image')
+const imageAttrs = computed(() => ({
+  ...(props.imgAttrs || {}),
+  ...(import.meta.server ? { onerror: 'this.setAttribute(\'data-error\', 1)' } : {}),
+}))
+
+onMounted(() => {
+  if (!imageRef.value) {
+    return
+  }
+
+  if (imageRef.value.complete && initialLoad) {
+    if (imageRef.value.getAttribute('data-error')) {
+      emit('error', new Event('error'))
+    } else {
+      emit('load', new Event('load'))
+    }
+  }
+
+  imageRef.value.onload = (event) => {
+    emit('load', event)
+  }
+
+  imageRef.value.onerror = (event) => {
+    emit('error', event)
+  }
 })
 
 const ui = computed(() => uploraImage({}))
